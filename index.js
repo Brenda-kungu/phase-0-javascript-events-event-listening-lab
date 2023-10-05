d6f0c36b64fde043efe950b2739fd4ef0ff84c76
Brenda <brendakungu3@gmail.com>
2023-10-05 11:21:18 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+ function addingEventListener() {
+
+    const input = document.getElementById('input')
+
+    alert('I have been clicked!')
 }
+
+input.addEventListener('click',addingEventListener);
